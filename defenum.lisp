5aa78a1e79a84942c79d20bcf3ad7631b1174351
Arthur Goldman <arthur@goldman-tribe.org>
2020-02-16 12:26:44 -0500

    c-style enums

--- defenum.lisp
+++ defenum.lisp
@@ -1,7 +1,7 @@
 (uiop:define-package :hindley-milner/defenum
     (:use :cl)
   (:nicknames :enum)
-  (:export :defenum :extend-enum))
+  (:export :defenum :extend-enum :def-c-enum))
 (cl:in-package :hindley-milner/defenum)
 
 (defmacro defenum (type-name common-slots variants)
@@ -33,3 +33,29 @@ this just defines a bunch of superclasses of ENUM-NAME."
                 :superclasses (,enum-name)))))
     `(progn
        ,@(mapcar #'define-variant variants))))
+
+(defmacro def-c-enum (name &rest variants)
+  "define a c-style enum, which associates several keywords with integer values.
+
+each of the VARIANTS should be either:
+- a keyword
+- a list of (KEYWORD VALUE), where VALUE is an integer.
+
+as in c, unsupplied values will increment by one from the last
+supplied value, starting at 0 if no value is supplied."
+  (let ((idx -1))
+    (flet ((variant-name (variant)
+             (etypecase variant
+               (keyword variant)
+               ((tuple keyword integer) (first variant))))
+           (variant-case-clause (variant)
+             (etypecase variant
+               (keyword (list variant (incf idx)))
+               ((tuple keyword integer)
+                (setf idx (second variant))
+                variant))))
+      `(progn (deftype ,name ()
+                '(member ,@(mapcar #'variant-name variants)))
+              (defun ,(gefjon-utils:symbol-concatenate name '-to-int) (,name)
+                (ecase ,name
+                  ,@(mapcar #'variant-case-clause variants)))))))
