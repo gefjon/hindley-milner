e82601a0f73797d184a951d900cfcbe36963c473
Phoebe Goldman <phoebe@goldman-tribe.org>
2020-08-08 15:31:00 -0500

    automate full compilation incl. linking

--- compile.lisp
+++ compile.lisp
@@ -1,6 +1,10 @@
 (uiop:define-package :hindley-milner/compile
-    (:nicknames :compile)
-  (:use :cl)
+  (:nicknames :compile)
+  (:mix :hindley-milner/prologue :cl)
+  (:import-from :uiop
+   :run-program)
+  (:import-from :asdf
+   :output-file :make-operation :compile-op :find-component :find-system)
   (:shadow :compile)
   (:import-from :hindley-milner/syntax
    :read-program-from-file)
@@ -15,12 +19,46 @@
   (:import-from :hindley-milner/llvm-emit
    :emit-to-file)
   (:export :compile))
-(cl:in-package :hindley-milner/compile)
+(in-package :hindley-milner/compile)
 
-(defun compile (infile outfile)
+(|:| #'compile-to-ll (-> (pathname pathname) void))
+(defun compile-to-ll (infile ll-outfile)
   (let* ((surface-syntax (read-program-from-file infile))
          (ir1 (ir1-trans surface-syntax))
          (cps (cps-trans ir1))
          (three-address (3adr-transform cps))
          (ir4 (ir4-trans three-address)))
-    (emit-to-file outfile ir4)))
+    (emit-to-file ll-outfile ir4))
+  (values))
+
+(defparameter *runtime-o-path*
+  (output-file (make-operation 'compile-op)
+               (find-component (find-system :hindley-milner/runtime)
+                               "main")))
+
+(|:| #'link (-> (pathname pathname) void))
+(defun link (ll-infile exe-outfile)
+  (run-program (list "clang" "-Wall" "-std=c11"
+                     "-o" (namestring exe-outfile)
+                     (namestring ll-infile)
+                     (namestring *runtime-o-path*))
+               :error-stream t))
+
+(|:| #'asmify (-> (pathname pathname) void))
+(defun asmify (ll-infile s-outfile)
+  (run-program (list "llc"
+                     "-o" (namestring s-outfile)
+                     (namestring ll-infile))))
+
+(|:| #'compile (-> ((or string pathname) (or string pathname)) void))
+(defun compile (infile outfile)
+  (let* ((source-path (pathname infile))
+         (exe-path (pathname outfile))
+         (s-path (merge-pathnames (make-pathname :type "s")
+                                  exe-path))
+         (ll-path (merge-pathnames (make-pathname :type "ll")
+                                   exe-path)))
+    (compile-to-ll source-path ll-path)
+    (asmify ll-path s-path)
+    (link s-path exe-path))
+  (values))
--- hindley-milner.asd
+++ hindley-milner.asd
@@ -1,5 +1,38 @@
+(uiop:define-package :hindley-milner.asd
+  (:mix :asdf :cl)
+  (:import-from :uiop
+   :if-let :run-program)
+  )
+(in-package :hindley-milner.asd)
+
+(defclass runtime-c-source-file (c-source-file) ())
+
+(defmethod output-files ((op compile-op) (c runtime-c-source-file))
+  (list (merge-pathnames (make-pathname :type "o")
+                         (component-pathname c))))
+
+(defun input-file (op c)
+  (if-let ((input-files (input-files op c)))
+    (progn (assert (= (length input-files) 1))
+           (first input-files))
+    (error "No input files for ~a on ~a" op c)))
+
+(defmethod perform ((op compile-op) (c runtime-c-source-file))
+  (run-program (list "clang" "-Wall" "--std=c11" "-c"
+                     "-o" (namestring (output-file op c))
+                     (namestring (input-file op c)))
+               :error-output t))
+
+(defmethod perform ((op load-op) (c runtime-c-source-file))
+  (declare (ignorable op c))
+  (values))
+
+(defsystem :hindley-milner/runtime
+  :pathname "runtime"
+  :components ((runtime-c-source-file "main")))
+
 (defsystem :hindley-milner
   :class :package-inferred-system
   :author "Phoebe Goldman"
   :version "0.0.1"
-  :depends-on (:hindley-milner/compile))
+  :depends-on (:hindley-milner/runtime :hindley-milner/compile))
